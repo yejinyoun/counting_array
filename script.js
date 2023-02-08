bcc150759af09eb3f37ac5fdd003dc5cbdf176cd
yejinyoun <yejinyoun@icloud.com>
2023-02-08 23:18:20 +0100

    counting and adding it on array

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 "use strict";
 
 console.log("hihi");
+
+const array = [];
+let counter = [];
+let i = 0;
+
+count();
+
+function count() {
+  array.unshift(i);
+  i++;
+
+  console.log(array, i);
+
+  setTimeout(count, 1000);
+}
